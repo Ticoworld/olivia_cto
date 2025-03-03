fba525b8a3ff1b2586fba05bccaf58be8aa56b04
Timothy Chinecherem <83535518+Ticoworld@users.noreply.github.com>
2025-03-03 06:28:39 +0100

    Update Footer.jsx
    X on footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => {
             Telegram
           </a>
           <a
-            href="https://x.com/oliviacommunity"
+            href="http://x.com/AigovCTO"
             target="_blank"
             rel="noopener noreferrer"
             className="text-gray-400 hover:text-white transition-colors duration-300 ease-in-out"
